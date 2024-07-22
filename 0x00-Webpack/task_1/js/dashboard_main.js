14818f1271846f0c678a9cfdb6da419f26529c55
Daniel <drewaju.16@gmail.com>
2024-07-22 22:50:43 +0100

    html

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -19,22 +19,3 @@ $('body').append("<p>Dashboard data for the students</p>");
 $('body').append($button);
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
-
-// import $ from 'jquery';
-// import _ from 'lodash';
-
-// $(document).ready(function() {
-//   $('body').append('<p>Holberton Dashboard</p>');
-//   $('body').append('<p>Dashboard data for the students</p>');
-//   $('body').append('<button>Click here to get started</button>');
-//   $('body').append('<p id="count"></p>');
-//   $('body').append('<p>Copyright - Holberton School</p>');
-
-//   let count = 0;
-//   function updateCounter() {
-//     count++;
-//     $('#count').text(`${count} clicks on the button`);
-//   }
-
-//   $('button').on('click', _.debounce(updateCounter, 500));
-// });
