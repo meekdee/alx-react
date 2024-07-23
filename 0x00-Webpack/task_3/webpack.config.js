b9ac299a2fe3bb10129d480aa055a4b4d5fe7568
Daniel <drewaju.16@gmail.com>
2024-07-23 02:11:42 +0100

    index

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -15,16 +15,26 @@ module.exports = {
     hints: false,
     maxEntrypointSize: 1000000,
   },
-  plugins: [ new CleanWebpackPlugin(), new HtmlWebpackPlugin() ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
   },
   devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
+    // Updated the devServer configuration to use the new 'static' property instead of 'contentBase'
+    static: {
+      directory: path.join(__dirname, 'public'), // Directory to serve static files from
+    },
+    compress: true, // Enable gzip compression for everything served
+    port: 8564, // Port number to run the development server on
+    open: true, // Open the browser after server has been started
   },
   output: {
     filename: '[name].bundle.js',
