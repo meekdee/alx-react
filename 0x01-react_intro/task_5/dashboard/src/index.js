cac98815d083e5df82f7de9dac4e84508fa39234
Daniel <drewaju.16@gmail.com>
2024-07-24 23:48:04 +0100

    omo

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 const App = () => {
-  return <div>Hello, Webpack and React!</div>;
+    return (
+        <div>Hello, Webpack and React!</div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
